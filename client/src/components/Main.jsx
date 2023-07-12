2f6b7dd8f199146a018acfe6f393c31836a29440
Rajdeep <rajdeep.hascode@gmail.com>
2023-07-12 12:09:26 +0530

    added run.sh, short-blog completion for mobile

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,5 +1,8 @@
 import Tag from "./Tag"
-const posts = []
+import ShortBlog from "./ShortBlog"
+const blogs = [{imageLink: 'https://miro.medium.com/v2/resize:fit:828/format:webp/1*jyZQjnQAlcoeNCQ8oBkujA.jpeg'}, 
+    {imageLink: 'https://miro.medium.com/v2/resize:fill:250:168/1*aqmtMRh9xnVwp6-5EEJ4KQ.png'}, 
+    {imageLink: 'https://miro.medium.com/v2/resize:fill:250:168/0*jxVHjlRyDYVgFuyE'}]
 
 export default function Main() {
     return (
@@ -7,23 +10,29 @@ export default function Main() {
             <aside className="home-aside">
                 <h3 className="home-aside-title">Discover more of what matters to you</h3>
                 <div className="tags">
-                    <Tag tagName='Programming'/>
+                    <Tag tagName='Programming' />
                     <Tag tagName='JavaScript' />
-                    <Tag tagName='Data Science '/>
+                    <Tag tagName='Data Science ' />
                     <Tag tagName='Tarvel' />
                     <Tag tagName='Writing' />
                     <Tag tagName='Productivity' />
                     <Tag tagName='Self Improvement' />
                     <Tag tagName='Machine Learning' />
                     <Tag tagName='Life' />
-
                 </div>
             </aside>
 
             <section className="short-blogs">
-                short blogs
+                {
+                    blogs.map((element, index) => {
+                        return <ShortBlog imageLink={element.imageLink} key={index} id={index} />
+                    })
+                }
+                {/* <ShortBlog imageLink={'https://miro.medium.com/v2/resize:fit:828/format:webp/1*jyZQjnQAlcoeNCQ8oBkujA.jpeg'} />
+                <ShortBlog imageLink={'https://miro.medium.com/v2/resize:fill:250:168/1*aqmtMRh9xnVwp6-5EEJ4KQ.png'} />
+                <ShortBlog imageLink={'https://miro.medium.com/v2/resize:fill:250:168/0*jxVHjlRyDYVgFuyE'} /> */}
             </section>
 
-        </main>    
+        </main>
     )
 }
---
+++ client/src/components/ShortBlog.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+const base = 'http://localhost:3000/'
+
+export default function ShortBlog(props) {
+
+    {/* A blog in short form  */ }
+    return <div className="short-blog">
+        {/* Author name, title, content etc.. */}
+        <div className="short-blog-left">
+            <Link className="short-blog-author-name link" to={`${base}r8`}>@r8</Link>
+            <h3 className="short-blog-title">Diary of an Interstellar Voyage, Report 35</h3>
+            <p className="short-blog-content">These aren’t travel tips, but ways of thinking about taking time off or going on vacation that can help level up your approach.</p>
+            <div className="short-blog-details">
+                <p className="date">Jul 8</p>
+                <div>.</div>
+                <Link className="tag-name link">Devolopment</Link>
+            </div>
+        </div>
+        {/* Blog Image */}
+        <picture className="short-blog-right">
+            <img src={props.imageLink} alt="blog image" className="short-blog-image" />
+        </picture>
+    </div>
+}
