a9956f90bb074163ba30caa59e514c15ca751e44
Rajdeep <rajdeep.hascode@gmail.com>
2023-08-14 17:19:20 +0530

    changes

--- client/src/helper/variables.js
+++ client/src/helper/variables.js
@@ -1,3 +1,3 @@
 export const base_url = 'http://localhost:3000/'
-// export const api_url = 'https://skeleton-server.onrender.com/'
-export const api_url = 'http://localhost:7000/'
+export const api_url = 'https://skeleton-server.onrender.com/'
+// export const api_url = 'http://localhost:7000/'
