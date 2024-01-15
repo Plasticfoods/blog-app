1997b7211e95ec763517d909e0100360f21e7762
Rajdeep <rajdeep.hascode@gmail.com>
2024-01-15 21:54:58 +0530

    Changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -41,7 +41,6 @@ router.get('/:username/posts', async (req, res) => {
         }
 
         const blogDocs = await Blog.find({username})
-        console.log(blogDocs)
 
         res.status(200).json({message: 'Success', blogs: blogDocs})
     }
