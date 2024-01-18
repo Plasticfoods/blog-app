803d19f30fb91e182aa63dfac312078836fe928d
Rajdeep <rajdeep.hascode@gmail.com>
2024-01-18 21:43:41 +0530

    Added corsOption in cors

--- server/server.js
+++ server/server.js
@@ -18,22 +18,22 @@ const postRouter = require('./routes/post')
 const userRouter = require('./routes/user')
 
 
-// var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:8080', 'http://localhost:8000']; //white list consumers
-// var corsOptions = {
-//     origin: function (origin, callback) {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(null, false);
-//         }
-//     },
-//     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-//     credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
-// };
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:8080', 'http://localhost:8000']; //white list consumers
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(null, false);
+        }
+    },
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
 
-app.use(cors({ origin: '*' }))
+app.use(cors(corsOptions))
 
 
 // Set CORS headers
